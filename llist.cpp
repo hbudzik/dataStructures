9efc46fe42d722b1e838a2ebff234911d737d82a
hbudzik <hubertbudmobile@gmail.com>
2021-12-22 13:30:27 -0600

    worked on llist.cpp

---
+++ llist.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ 
+ int x;
+ 
+  cout << "select an option\n";
+  cout << "1: print out\n";
+  cout << "2: exit\n";
+  cout << ">";
+  cin >> x;
+
+  if  (x == 1)
+  {
+	  cout << "test 1";
+	  cout << "\n";
+	  cout << "yo yo";
+  }else{
+   	  cout << "exitting .... ";
+  } 
+
+return 0;
+}
